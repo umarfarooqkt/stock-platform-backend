8a69601c2e34e271a0dcf05019494d5d72dd5cea
Jordan Mathewson <jmathe53@uwo.ca>
2020-03-31 21:43:02 -0400

    Remove frontend subdomain parameter from template

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,8 @@ STACK_NAME=stock-platform-backend
 BUILD_BUCKET=cs4471-group1-project-backend-builds
 FRONTEND_BUCKET=cs4471-group1-project-frontend
 FRONTEND_DOMAIN=cs4471-stock-platform.xyz
-FRONTEND_LOGIN_SUBDOMAIN=cs4471-group1-project-login
 
 sam build
 sam deploy --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --confirm-changeset --s3-bucket $BUILD_BUCKET \
   --parameter-overrides ParameterKey=FrontendBucketName,ParameterValue=$FRONTEND_BUCKET \
-  ParameterKey=FrontendDomain,ParameterValue=$FRONTEND_DOMAIN \
-  ParameterKey=FrontendLoginSubDomain,ParameterValue=$FRONTEND_LOGIN_SUBDOMAIN
+  ParameterKey=FrontendDomain,ParameterValue=$FRONTEND_DOMAIN
