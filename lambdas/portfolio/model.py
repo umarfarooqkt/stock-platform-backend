5a26d25e14b4e86a713c3e7cf80a406fc3733f2e
Spencer Ridley <sridley4@uwo.ca>
2020-04-01 21:51:22 -0400

    Update model.py

--- lambdas/portfolio/model.py
+++ lambdas/portfolio/model.py
@@ -46,6 +46,3 @@ class Favourite(Base):
             "stock_name" : self.stock_name,
             "stock_description" : self.stock_description
         }
-
-import database_manager
-Base.metadata.create_all(database_manager.engine)
